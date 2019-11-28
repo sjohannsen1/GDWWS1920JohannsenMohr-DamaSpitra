b74a34f5f31292cc465ea525f2699938a689b426
Sophia Johannsen <sophia_ella_margarethe.johannsen@smail.th-koeln.de>
2019-11-28 18:49:04 +0100

    API Test 1, ka was da abgeht

---
+++ test/webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    mode: "development",
+    entry: "./test.js",
+    output: {
+      path: "C:\gdw_uebungen\Sophia\GDWWS1920JohannsenMohr-DamaSpitra",
+      filename: "bundle.js"
+    },
+    devServer: {
+      contentBase: "./"
+    }
+  };

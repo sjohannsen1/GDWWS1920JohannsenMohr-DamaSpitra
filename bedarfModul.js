677ce490f9c7e5c5a1b22f76c4ec630b58df8d5a
Sophia Johannsen <sophia_ella_margarethe.johannsen@smail.th-koeln.de>
2020-01-13 13:02:14 +0100

    bsp rezept added

--- bedarfModul.js
+++ bedarfModul.js
@@ -1,8 +1,10 @@
 const calBedarf=(height, weight, sex, activity, age)=>{
     if(sex=="m")
         return (66.47+(13.7*weight)+(5*height)-(6.8*age))*(activity)
-     else
-        return (65.51+(9.6*weight)+(1.8*height)-(4.7*age))*(activity)
+     else if (sex=="w")
+        return (655.1+(9.6*weight)+(1.8*height)-(4.7*age))*(activity)
+    else 
+        return 0 
 }
 const proBedarf=(weight, age)=>{
     if(age<65)
